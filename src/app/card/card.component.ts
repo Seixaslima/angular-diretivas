0eba9d7d196559c27069ce094aef8591e8596cef
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-01-03 11:47:50 -0300

    diretiva estrutural ngFor

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
+  imports: [CommonModule]
 })
 export class CardComponent {
+  produtos: string[] = []
+
+  constructor() {
+    this.produtos = [
+      "Banana",
+      "Maça",
+      "Laranja",
+      "Pera",
+      "Mamão"
+    ]
+  }
 
 }
