3f613e7a73e0e3f647effe3b02b5cc4777ce9404
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-01-03 15:25:40 -0300

    diretiva de atributo ngModel

--- src/app/comp-atributo/comp-atributo.component.ts
+++ src/app/comp-atributo/comp-atributo.component.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-comp-atributo',
   standalone: true,
   templateUrl: './comp-atributo.component.html',
   styleUrl: './comp-atributo.component.css',
-  imports: [CommonModule]
+  imports: [CommonModule, FormsModule]
 })
 export class CompAtributoComponent {
 
   estilo:string = "enable";
   corDeFundo:string = 'blue';
-  corDaLetra:string = 'yellow'
+  corDaLetra:string = 'yellow';
+  item:string = '';
+  listaItem: string[] = []
 
   trocar() {
     if(this.estilo === "enable") {
@@ -20,6 +23,11 @@ export class CompAtributoComponent {
       return
     }
     this.estilo = 'enable'
+  }
+
+  adicionar() {
+    this.listaItem.push(this.item)
+    this.item = ''
   }
 
 }
